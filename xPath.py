efa6a9874dae5a5a0fe223c1e4c531f66682fb6a
beatrizsnichelotto <78983638+beatrizsnichelotto@users.noreply.github.com>
2021-04-29 17:14:33 -0300

    Add files via upload

---
+++ xPath.py
@@ -0,0 +1,23 @@
+#Caminhos de login
+   
+signin = '//*[@id="__next"]/header/div[2]/div/div[3]/div/div/a[1]'
+username = '//*[@id="__next"]/main/section/div/div/div/div/form/div/div[1]/div/input'
+signinPassword = '//*[@id="__next"]/main/section/div/div/div/div/form/div/div[2]/div/input'
+showSigninPassword = '//*[@id="__next"]/main/section/div/div/div/div/form/div/div[2]/div/button'
+login = '//*[@id="__next"]/main/section/div/div/div/div/form/div/div[4]/button'
+
+
+#Caminhos de cadastro
+
+signUp = '//*[@id="__next"]/header/div[2]/div/div[3]/div/div/a[2]'
+cadEmail = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[1]/div/input'
+cadSenha = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[2]/div/input'
+showSigninPasswordCad = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[2]/div/button'
+cadDDD = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[3]/div[1]/div/div/input'
+cadTelefone = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[3]/div[2]/div/div/input'
+cadNome = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[4]/div/input'
+cpf = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[5]/div[1]/div/div/input'
+cadData = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[2]/div[5]/div[2]/div/div/input'
+cadastrar = '//*[@id="__next"]/main/section/div/div/div/div/div/form/div[3]/button'
+
+#Venda
